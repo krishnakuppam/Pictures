ae48e83ec9686c6b4e503f09b32b7cd88076fa97
kuppamkk <krishnakuppam38@gmail.com>
2025-08-21 17:38:24 +0530

    day21

---
+++ Desktop/images/my.portfolios/index.html/.py
@@ -0,0 +1,10 @@
+num = str(input("Enter a name: "))
+num1 = str(input("Enter another name: "))
+
+if num == num1:
+    print("name is okay.")
+elif num != num1:
+    print("name is not okay.")
+
+else:
+    print(" i can't find what you entered.")
