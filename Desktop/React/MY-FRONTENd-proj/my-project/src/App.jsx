1530e54a7b4a41c085fcb87aba01826c054930a7
Krishna R A <krishnakuppam38@gmail.com>
2025-10-11 14:54:20 +0530

    day

--- Desktop/React/MY-FRONTENd-proj/my-project/src/App.jsx
+++ Desktop/React/MY-FRONTENd-proj/my-project/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './pages/Navbar'
 
 const App = () => {
   return (
     <div>
-      
+         <Navbar />
     </div>
   )
 }
---
+++ Desktop/React/MY-FRONTENd-proj/my-project/src/pages/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+
+        <div className='flex justify-between items-center bg-gray-200 p-2'>
+            <h1 className='text-2xl font-bold'>Fashion Store</h1>
+            <div className='flex gap-4'>
+                
+
+            </div>
+
+        </div>
+           
+    </div>
+  )
+}
+
+export default Navbar
