440d6719bad4266d45fa08ca3b4a6efbce96f9a1
Krishna R A <krishnakuppam38@gmail.com>
2025-12-16 17:56:05 +0530

    nov 16

--- Desktop/portfolio/frontend/script.js
+++ Desktop/portfolio/frontend/script.js
@@ -1,10 +1,33 @@
-// Name rotation is handled by CSS animation
-// This script file can be used for other interactive features
 
-  const text = document.getElementById("eyebrow");
-  const letters = text.innerText.split("");
 
-  text.innerHTML = letters
-    .map(letter => `<span>${letter}</span>`)
-    .join("");
+  // const text = document.getElementById("eyebrow");
+  // const letters = text.innerText.split("");
+
+  // text.innerHTML = letters
+  //   .map(letter => `<span>${letter}</span>`)
+  //   .join("");
+
+
+   function updateClock() {
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let ampm = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12 || 12;
+
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  document.getElementById("clock").innerText =
+    `${hours}:${minutes}:${seconds} ${ampm}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+
 
--- Desktop/practice Folder/JS Pojects/Clock Project/script.js
+++ Desktop/practice Folder/JS Pojects/Clock Project/script.js
@@ -1,11 +1,10 @@
 function  updateClock() {
-
     const now = new Date();
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
     let ampm = "AM";
-    let DateString = now.toDateString();
+    
 
 
 
@@ -20,7 +19,7 @@ function  updateClock() {
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    const timestring = `${hours}:${minutes}:${seconds} ${ampm} - ${DateString}`;
+    const timestring = `${hours}:${minutes}:${seconds} ${ampm}`;
 
     document.getElementById("clock").textContent = timestring;
 
